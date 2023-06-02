73b11c71471b58b3f2fa795ea9caac0856effcce
Jacopo <49444372+jacopodona@users.noreply.github.com>
2023-06-02 18:44:55 +0200

    added model save

--- RL/main.py
+++ RL/main.py
@@ -1,3 +1,6 @@
+import pickle
+import random
+
 import gymnasium as gym
 from gymnasium.wrappers import RecordVideo
 
@@ -12,7 +15,7 @@ from stable_baselines3 import DQN
 
 from matplotlib import animation
 
-NUMBER_EPISODES = 1000  # Basically number of epochs
+NUMBER_EPISODES = 4000  # Basically number of epochs
 TIMESTEPS = 100
 
 
@@ -22,20 +25,18 @@ def renderEnvironmentExample(env):
     env.close()
 
 def randomChoiceDemo(env):
-    for e in range(NUMBER_EPISODES):
-        initial_state = env.reset()
-        print(e)
-        # env.render()
-        appendedObservations = []
-        for timeIndex in range(TIMESTEPS):
-            # print(timeIndex)
-            random_action = env.action_space.sample()
-            observation, reward, terminated, truncated, info = env.step(random_action)
-            appendedObservations.append(observation)
-            if (terminated):
-                time.sleep(0.5)
-                # env.close()
-                break
+    observation = env.reset()[0].flatten()
+    frames=[]
+    done = False
+    truncated = False
+    actions=[0,1,2,3,4]
+    while (not done) and (not truncated):
+        action = random.choice(actions)
+        observation_, reward, done, truncated, info = env.step(action)
+        observation_ = observation.flatten()
+        observation = observation_
+        frames.append(env.render())
+    save_frames_as_gif(frames=frames, filename="demo.gif")
 
 
 def list_envs():
@@ -58,6 +59,31 @@ def save_frames_as_gif(frames, path='./run/', filename='gym_animation.gif'):
     anim.save(path + filename, writer='imagemagick', fps=60)
 
 
+def showDemo(agent, env):
+    for i in range(10):
+        frames=[]
+        score=0
+        observation = env.reset()[0].flatten()
+        done = False
+        truncated = False
+        while (not done) and (not truncated):
+            action = agent.choose_action(observation)
+            observation_, reward, done, truncated, info = env.step(action)
+            observation_ = observation.flatten()
+            score += reward
+            agent.store_transition(state=observation, action=action, reward=reward, state_=observation_, done=done)
+            agent.learn()
+            observation = observation_
+            frames.append(env.render())
+        save_frames_as_gif(frames=frames, filename=str(i)+"_best_agent_visualized_.gif")
+
+
+def save_agent(best_agent):
+    with open('./run/best_agent.pkl', 'wb') as outp:
+        pickle.dump(best_agent, outp, pickle.HIGHEST_PROTOCOL)
+    torch.save(best_agent.Q_eval.state_dict(), "best_agent_q_eval.pt")
+
+
 def learnDQNetwork():
     wandb.init(project='Highway-DQN')
     config = {
@@ -72,7 +98,8 @@ def learnDQNetwork():
         "lanes_count": 3,
         "initial_spacing": 2,
         "collision_reward": -5,  # The reward received when colliding with a vehicle.
-        "reward_speed_range": [20, 30]
+        "reward_speed_range": [20, 30],
+        "normalize_reward":False
     }
     env = gym.make("highway-fast-v0", render_mode='rgb_array')
     # Wrap the env by a RecordVideo wrapper
@@ -83,15 +110,17 @@ def learnDQNetwork():
     # so it can send it intermediate simulation frames.
     # env.unwrapped.set_record_video_wrapper(env)
     env.configure(config)
-    agent = Agent(gamma=0.9, epsilon=1.0, batch_size=128, n_actions=5, eps_end=0.01, input_dims=90, lr=5e-5)
+    agent = Agent(gamma=0.9, epsilon=1.0, batch_size=128, n_actions=5, eps_end=0.01, input_dims=90, lr=5e-5,eps_dec=3e-5)
     scores, epshistory = [], []
+    best_score=0
+    best_agent=None
 
     for i in range(NUMBER_EPISODES):
         score = 0
         done = False
         truncated=False
         frames = []
-        observation = env.reset(seed=1)[0].flatten()
+        observation = env.reset()[0].flatten()
         while (not done) and (not truncated):
             action = agent.choose_action(observation)
             observation_, reward, done, truncated, info = env.step(action)
@@ -100,17 +129,25 @@ def learnDQNetwork():
             agent.store_transition(state=observation, action=action, reward=reward, state_=observation_, done=done)
             agent.learn()
             observation = observation_
-            #frames.append(env.render())
+            frames.append(env.render())
         scores.append(score)
         epshistory.append(agent.epsilon)
-        avg_score = np.mean(scores[-100:])
+        #avg_score = np.mean(scores[-100:])
         #save_frames_as_gif(frames=frames, filename="episode" + str(i) + ".gif")
+
+        if score>best_score:
+            best_score=score
+            best_agent=agent
+
 
         print('episode ', i,
               '\n\tscore=', score,
               '\n\tepsilon=', agent.epsilon)
 
         wandb.log({"epidose":i,"score": score, "epsilon": agent.epsilon})
+
+    showDemo(best_agent, env)
+    save_agent(best_agent)
 
     wandb.finish()
 
@@ -120,7 +157,10 @@ def learnDQNetwork():
 
 if __name__ == '__main__':
     print(torch.cuda.is_available())
-    # list_envs()
+    list_envs()
 
+    #env = gym.make("highway-v0", render_mode='rgb_array')
+    #randomChoiceDemo(env)
     learnDQNetwork()
 
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
