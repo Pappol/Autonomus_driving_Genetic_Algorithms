23410f3118a8fab93b6233fbbb9eadb874c42430
Jacopo <49444372+jacopodona@users.noreply.github.com>
2023-07-09 11:39:44 +0200

    plotting results

--- plot_results_csv.py
+++ plot_results_csv.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import wandb
 
 gens=list(range(1,101)) #For evolutionary strategies
 iterations=list(range(1,4000)) #For DQN
@@ -8,7 +9,20 @@ iterations=list(range(1,4000)) #For DQN
 df=pd.read_csv("RL/delivery_plot.csv")
 df=df.rename(columns={'delivery run - score': 'score'})
 df['score'] = df['score'].astype(float)
-
 df.plot(x='Step',y='score',linewidth=0.2)
+plt.grid(axis="y")
+
+#For CMA_ES
+#df=df.rename(columns={'delivery run - score': 'score'})ù
+api = wandb.Api()
+run3 = api.run("/jacopodona/highway_CMA/runs/pw5opfhv") #for cma es 3
+run1 = api.run("/jacopodona/highway_CMA/runs/nnqio6qf") #for cma es 1
+df3=run3.history()
+df1=run1.history()
+#
+df3.plot(x="Generation",y=["Best Fitness","Median Fitness"])
+plt.grid(axis="y")
+df1.plot(x="Generation",y=["Best Fitness","Median Fitness"])
+plt.grid(axis="y")
 plt.legend()
 plt.show()
